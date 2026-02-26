65eb289441918668dfd83e963a081e5f21e9ac6d
prajwalnimblix <prajwalnimblix@gmail.com>
2026-02-26 09:22:28 +0530

    Update PatientRepository.java

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/repository/PatientRepository.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/repository/PatientRepository.java
@@ -1,20 +1,12 @@
 package nimblix.in.HealthCareHub.repository;
 
-<<<<<<< Prajwal-S-R
-import org.springframework.data.jpa.repository.JpaRepository;
-import nimblix.in.HealthCareHub.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
-=======
 import nimblix.in.HealthCareHub.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-    // Get only active patients
-    List<Patient> findByIsDeletedFalse();
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    // Get only active (non-deleted) patients
+    List<Patient> findByIsDeletedFalse();
 }
->>>>>>> main
